f1dc8b7a18e78c43f0dea3e3090b35d1bfc482dc
jack281291 <antonio.lisi1991@libero.it>
2020-02-07 14:44:14 +0000

    finished logistic

--- pytorch_basic/3_logistic_regression_with_real_data.py
+++ pytorch_basic/3_logistic_regression_with_real_data.py
@@ -1,1 +1,47 @@
 from torch import nn, optim, from_numpy
+import numpy as np
+
+xy = np.loadtxt('./data/diabetes.csv.gz', delimiter=',', dtype=np.float32)
+x_data = from_numpy(xy[:, 0:-1])
+y_data = from_numpy(xy[:, [-1]])
+
+print(f'X\'s shape: {x_data.shape} | Y\'s shape: {y_data.shape}')
+
+
+class Model(nn.Module):
+    def __init__(self):
+        super(Model, self).__init__()
+        self.l1 = nn.Linear(8, 6)
+        self.l2 = nn.Linear(6, 4)
+        self.l3 = nn.Linear(4, 1)
+
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self, x):
+        out1 = self.sigmoid(self.l1(x))
+        out2 = self.sigmoid(self.l2(out1))
+        y_pred = self.sigmoid(self.l3(out2))
+        return y_pred
+
+# our model
+model = Model()
+
+
+# Construct our loss function and an Optimizer. The call to
+# model.parameters()
+# in the SGD constructor will contain the learnable parameters of the two
+# nn.Linear modules which are members of the model.
+criterion = nn.BCELoss(reduction='mean')
+optimizer = optim.SGD(model.parameters(), lr=0.1)
+
+# Training loop
+for epoch in range(100):
+    y_pred = model(x_data)
+
+    loss = criterion(y_pred, y_data)
+    print(f'Epoch: {epoch + 1}/100 | Loss: {loss.item():.4f}')
+
+    # Zero gradients, perform a backward pass, and update the weights.
+    optimizer.zero_grad()
+    loss.backward()
+    optimizer.step()
